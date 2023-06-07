865acf770b4cb043ccf4d1fd984fd1fc1524b30f
jirisimonov <jirimonov@gmail.com>
2023-06-07 07:38:47 +0300

    mt-7 refactor: rename products component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,12 +4,12 @@ import { RouterModule } from '@angular/router'
 import { RxFor } from '@rx-angular/template/for'
 import { ProductsComponent } from './products.component'
 import { productsRoutes } from './products.routes'
-import { ProductCardsComponent } from './components/product-cards/product-cards.component'
+import { ProductsListComponent } from './components/product-cards/products-list.component'
 import { SharedModule } from '../shared/shared.module'
 import { ProductCardComponent } from './components/product-card/product-card.component'
 
 @NgModule({
-  declarations: [ProductsComponent, ProductCardsComponent, ProductCardComponent],
+  declarations: [ProductsComponent, ProductsListComponent, ProductCardComponent],
   imports: [CommonModule, RouterModule.forChild(productsRoutes), RxFor, NgOptimizedImage, SharedModule],
 })
 export class ProductsModule {}
